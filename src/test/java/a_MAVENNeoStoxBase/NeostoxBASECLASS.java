a3bcda0c02779291b2e470abbf01a2cf5ed248f7
remo14321 <121381098+remo14321@users.noreply.github.com>
2022-12-26 00:23:01 +0530

    Update NeostoxBASECLASS.java
    added spaces

--- src/test/java/a_MAVENNeoStoxBase/NeostoxBASECLASS.java
+++ src/test/java/a_MAVENNeoStoxBase/NeostoxBASECLASS.java
@@ -12,9 +12,13 @@ public class NeostoxBASECLASS {
 	public void launchbrowser1() {
 		
 		System.setProperty("webdriver.chrome.driver", "D:\\chrome driver\\chromedriver.exe");	
+		
 		WebDriver driver = new ChromeDriver();
+		
 		driver.get("https://neostox.com/");
+		
 		driver.manage().window().maximize();
+		
 		driver.manage().timeouts().implicitlyWait(Duration.ofMillis(1000));
 	}
 
